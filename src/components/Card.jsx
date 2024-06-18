7aac3b81098a586fff63318f50db69d69724dffd
salunkemanas <123manassalunke@gmail.com>
2024-06-18 16:27:40 +0530

    $

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -80,7 +80,7 @@ const Card = ({selectedOption}) => {
                   />
                 </div>
                 <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
               </Link>
             ))}
           </div>
